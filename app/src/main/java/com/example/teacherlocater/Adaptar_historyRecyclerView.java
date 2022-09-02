334a1604ccf99913217a0ce41ac36ed2039ad5c1
Dany.obscure.tech <Dany.obscure.tech@gmail.com>
2022-09-02 08:09:01 +0500

    First Commit

--- app/src/main/java/com/example/teacherlocater/Adaptar_historyRecyclerView.java
+++ app/src/main/java/com/example/teacherlocater/Adaptar_historyRecyclerView.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Adaptar_historyRecyclerView extends RecyclerView.Adapter<Adaptar_historyRecyclerView.ViewHolder>{
@@ -35,6 +38,33 @@ public class Adaptar_historyRecyclerView extends RecyclerView.Adapter<Adaptar_hi
         holder.time_textView.setText(timeList.get(position));
         if (statusList.get(position).equals("entered")){
             holder.relativeLayout.setBackgroundResource(R.drawable.cyan_gradient);
+        }else {
+            if (position !=0){
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MMMM/yyyy 'at' hh:mm:ss a");
+//                SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("hh:mm a");
+
+                Date date1 = null;
+                try {
+                    date1 = simpleDateFormat.parse(timeList.get(position-1));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Date date2 = null;
+                try {
+                    date2 = simpleDateFormat.parse(timeList.get(position));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                long difference = date2.getTime() - date1.getTime();
+                int days = (int) (difference / (1000*60*60*24));
+                int hours = (int) ((difference - (1000*60*60*24*days)) / (1000*60*60));
+                int min = (int) (difference - (1000*60*60*24*days) - (1000*60*60*hours)) / (1000*60);
+                hours = (hours < 0 ? -hours : hours);
+                holder.time_textView.setText(timeList.get(position)+"\n"+"Time Spent: "+hours+" Hours and "+min+" Minutes");
+                System.out.println(min);
+            }
+
         }
     }
 
